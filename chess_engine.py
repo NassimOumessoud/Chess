bc0b3cf6fedb2ce84e55191d2885874e2537e772
Nassim <nassim.oumes@gmail.com>
2021-12-28 17:04:28 +0100

    Correct legal moves.

--- chess_engine.py
+++ chess_engine.py
@@ -8,15 +8,43 @@ import click
 
 
 class Board():
-    """Class that, given the name and starting position of a chess piece 
-       can determine its possible future positions and move the piece. 
     """
-    def __init__(self):
-        self.game = self.board()
+    Chess engine to initialize standard and custom chess games.
+    """
+    def __init__(self, mode='standard'):
 
-    def __str__(self):
-        print(self.game)
-        return self.game
+        if mode == 'standard':
+            self.game = self.board()
+            self.colors = ['W', 'B']
+            
+        else:
+            pass
+            
+
+    #def __str__(self):
+    #    print(self.game)
+    #    return self.game
+
+
+    def board(self):
+        """
+        Creates a standard chess board with pieces in place, white pieces on the bottom columns.
+        """
+        SQUARES = 8
+        grid = np.full((SQUARES, SQUARES), '--', dtype=np.dtype('U100'))
+
+        pawns = self.placer([i for i in range(8)], ['Bp', 'Wp'], AMOUNT=8)
+        rooks = self.placer([0, 7], ['Br', 'Wr'])
+        horses = self.placer([1, 6], ['Bh', 'Wh'])
+        bishops = self.placer([2, 5], ['Bb', 'Wb'])
+        queens = self.placer(3, ['BQ', 'WQ'], AMOUNT = 1)
+        kings = self.placer(4, ['BK', 'WK'], AMOUNT = 1)
+        starts = pawns + rooks + horses + bishops + queens + kings
+
+        for start in starts:
+            grid[start[0], start[1]] = start[2]
+
+        return grid
 
 
     def placer(self, displace, names, AMOUNT=2):
@@ -49,50 +77,35 @@ class Board():
                     positions.append([y, x, name])
                     
         return positions
-     
-       
-    def board(self):
-        SQUARES = 8
-        grid = np.full((SQUARES, SQUARES), '--', dtype=np.dtype('U100'))
-
-        pawns = self.placer([i for i in range(8)], ['Bp', 'Wp'], AMOUNT=8)
-        rooks = self.placer([0, 7], ['Br', 'Wr'])
-        horses = self.placer([1, 6], ['Bh', 'Wh'])
-        bishops = self.placer([2, 5], ['Bb', 'Wb'])
-        queens = self.placer(3, ['BQ', 'WQ'], AMOUNT = 1)
-        kings = self.placer(4, ['BK', 'WK'], AMOUNT = 1)
-        starts = pawns + rooks + horses + bishops + queens + kings
-
-        for start in starts:
-            grid[start[0], start[1]] = start[2]
-
-        return grid
          
 
-    def legal_moves(name, position):
+    def diagonal(self, location, vars=7):
+            options  = []
+
+
+            for var in range(-vars, vars + 1):
+                xd = location[1] - var
+                yd = location[0] + var
+                
+                xm = location[1] + var
+                ym = location[0] + var
+
+                if (0 <= xd < 8) and (0 <= yd < 8):
+                    options.append([yd, xd])
+                
+                if (0 <= xm < 8) and (0 <= ym < 8):
+                    options.append([ym, xm])
+
+            return options
+
+
+    def legal_moves(self, name, position):
         """Function that for each chess piece determines the possible spots it 
         can move to."""
         
         tiles = range(8)
         NAME = name[1]
-        
-        def diagonal(location, var):
-            xd = location[1] - var
-            yd = location[0] + var
-            
-            xm = location[1] + var
-            ym = location[0] + var
-                    
-            if (0 <= xd < 8) and (0 <= yd < 8):
-                if (0 <= xm < 8) and (0 <= ym < 8):
-                    return [[yd, xd], [ym, xm]]
-            
-            if (0 <= xd < 8) and (0 <= yd < 8):
-                return [yd, xd]
-            
-            if (0 <= xm < 8) and (0 <= ym < 8):
-                return [ym, xm]
-            
+
 
         if NAME == 'p':
             if name[0] == 'W':
@@ -104,11 +117,10 @@ class Board():
                             [position[0] + 2, position[1]]]
         
     
-        if NAME == 'r':
-            options = [[[tile, position[1]] for tile in tiles],
-                      [[position[0], tile] for tile in tiles]]
-                       
+        if NAME == 'b':
+            options = self.diagonal(position)
             
+
         if NAME == 'h':
             displace = [-2, -1, 1, 2]
             options = []
@@ -126,12 +138,12 @@ class Board():
                         continue
             
             
-        if NAME == 'b':
+        if NAME == 'r':
             options = []
             for tile in tiles:
                 options.append([position[0], tile])
                 options.append([tile, position[1]])
-                
+
                 
         if NAME == 'Q':
             options = []
@@ -139,75 +151,59 @@ class Board():
             for tile in tiles:
                 options.append([position[0], tile])
                 options.append([tile, position[1]])
-        
-            for tile in range(-tiles[-1], tiles[-1] + 1):
-                print(tile)
-                xd = position[1] - tile
-                yd = position[0] + tile
-                
-                xm = position[1] + tile
-                ym = position[0] + tile
-                
-                if (0 <= xd < 8) and (0 <= yd < 8):
-                    options.append([yd, xd])
+            diagonal_options = self.diagonal(position)
             
-                if (0 <= xm < 8) and (0 <= ym < 8):
-                    options.append([ym, xm])
-                    
+            options.extend(diagonal_options)
+                        
                         
         if NAME == 'K':
             options = []
-            signs = [-1, 1]
-            for i in signs:
-                xd = position[1] - i
-                yd = position[0] + i
-                
-                xm = position[1] + i
-                ym = position[0] + i
-                
-                if (0 <= xd < 8) and (0 <= yd < 8):
-                    options.append([yd, xd])
-            
-                if (0 <= xm < 8) and (0 <= ym < 8):
-                    options.append([ym, xm])
-                    
+ 
+            for i in [-1, 1]:
                 if (0 <= position[1] + i < 8):
                     options.append([position[0], position[1] + i])
                 
                 if (0 <= position[0] + i < 8):
                     options.append([position[0] + i, position[1]])
-                    
+
+            diagonal_options = self.diagonal(position, vars=1)
+            options.extend(diagonal_options)
+            
+          
         return options
     
     
     def move(self):
+
+
         @click.command()
         @click.option('--positions', prompt='What will be your next move?', nargs=2, type=str, help='Type two positions in chess format. The piece on position 1 will move to position 2.')
         def movement(positions):
             letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-            positions = [pos for pos in positions]
+            positions = [position for position in positions]
 
-            print(f"Received positions: {positions}")
             for index, pos in enumerate(positions):
                 if pos.lower() in letters:
                     pos = int(letters.index(pos))
                     positions[index] = pos
 
-            print(f"Modified positions: {positions}")
             old = [8 - int(positions[1]), int(positions[0])]
             new = [8 - int(positions[-1]), int(positions[-2])]
-            print(old, new)
-            
             name = self.game[old[0], old[1]]
-            
-            if new in Board.legal_moves(name, old):    
-                self.game[new[0], new[1]] = name
-                self.game[old[0], old[1]] = '--'
-                print(self.game)
-                return self.move()
-            else:
-                print('This is not a valid move')
-                return self.move()
+            if name[0] != self.turn:
+                print('It is not your turn, please wait until the other player has made a move!')
+            options = Board.legal_moves(self, name, old)
+
+            for option in options:
+                if option == new:
+                    self.game[new[0], new[1]] = name
+                    self.game[old[0], old[1]] = '--'
+    
+                    print(self.game)
+                    return self.move()
+
+            print("This is not a valid move, please type: circle(position) to see all valid moves for a piece on a position.")
+            return self.move()
         movement()
     
 
